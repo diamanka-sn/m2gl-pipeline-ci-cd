bf226f52a5f8278d6391704bd336196034d70a5a
Mouhamadou Diamanka <diamanka1998@gmail.com>
2023-03-15 03:22:43 +0100

    changements

--- src/main/java/sn/cicd/m2glcicd/M2glCicdApplication.java
+++ src/main/java/sn/cicd/m2glcicd/M2glCicdApplication.java
@@ -2,6 +2,7 @@ package sn.cicd.m2glcicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,4 +17,11 @@ public class M2glCicdApplication {
 		SpringApplication.run(M2glCicdApplication.class, args);
 	}
 
+// 	echo "# m2gl-pipeline-ci-cd" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/diamanka-sn/m2gl-pipeline-ci-cd.git
+// git push -u origin main
 }
